869a725f62f14d5b3566c96972cabcc374a02ebd
Amir Inbar <amirinbarcode@gmail.com>
2022-07-30 13:01:05 +0300

    Updates

--- src/models/Contact/Contact.tsx
+++
@@ -1,9 +0,0 @@
-export interface Contact {
-  chats: string[];
-  lastseen:number;
-  displayname: string | null;
-  img: string;
-  status: string | null;
-  number: string | null;
-  _id: string;
-}
--- src/models/Msg/Msg.tsx
+++
@@ -1,9 +0,0 @@
-export interface Msg {
-    body:string;
-    createdAt:number;
-    displayname:string | null;
-    id:string;
-    senderId:string;
-    status:string;
-    type:string
-}
